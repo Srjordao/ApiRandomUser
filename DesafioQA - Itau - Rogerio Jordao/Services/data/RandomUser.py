3d6c68a798e6fb63df31bdb1b9909cbb9122d7ab
Rogério Jordão <ro_jordao@hotmail.com>
2024-03-20 10:49:16 -0300

    Add files via upload

---
+++ DesafioQA - Itau - Rogerio Jordao/Services/data/RandomUser.py
@@ -0,0 +1,67 @@
+def consultar_resultados():
+    return {
+        "results": [
+            {
+                "gender": "",
+                "name": {
+                    "title": "",
+                    "first": "",
+                    "last": ""
+                },
+                "location": {
+                    "street": {
+                        "number": "",
+                        "name": ""
+                    },
+                    "city": "",
+                    "state": "",
+                    "country": "",
+                    "postcode": "",
+                    "coordinates": {
+                        "latitude": "",
+                        "longitude": ""
+                    },
+                    "timezone": {
+                        "offset": "",
+                        "description": ""
+                    }
+                },
+                "email": "",
+                "login": {
+                    "uuid": "",
+                    "username": "",
+                    "password": "",
+                    "salt": "",
+                    "md5": "",
+                    "sha1": "",
+                    "sha256": ""
+                },
+                "dob": {
+                    "date": "",
+                    "age": ""
+                },
+                "registered": {
+                    "date": "",
+                    "age": ""
+                },
+                "phone": "",
+                "cell": "",
+                "id": {
+                    "name": "",
+                    "value": ""
+                },
+                "picture": {
+                    "large": "",
+                    "medium": "",
+                    "thumbnail": ""
+                },
+                "nat": ""
+            }
+        ],
+        "info": {
+            "seed": "",
+            "results": 1,
+            "page": 1,
+            "version": ""
+        }
+    }
